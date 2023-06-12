af787ced222a34acbf5700a535c1fc89f6f010e5
AssanCred <temix@live.ru>
2023-06-12 17:52:08 +0700

    Initial Commit

---
+++ ColorPicker_SwiftUI/ColorPicker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPicker_SwiftUIApp.swift
+//  ColorPicker_SwiftUI
+//
+//  Created by Артём Латушкин on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorPicker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
